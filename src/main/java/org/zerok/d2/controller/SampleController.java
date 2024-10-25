5641e1ec51a233f65e0c5746c2b0206b7a51f8e6
shongCoder <soyeong163@gmail.com>
2024-10-25 11:23:16 +0900

    Initial commit

---
+++ src/main/java/org/zerok/d2/controller/SampleController.java
@@ -0,0 +1,2 @@
+package org.zerok.d2.controller;public class SampleController {
+}
---
+++ src/test/java/org/zerok/d2/D2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerok.d2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
