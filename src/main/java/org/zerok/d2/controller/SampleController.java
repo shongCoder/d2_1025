c3e621a4a1b3b072bc252ec90c8c7ba0f4089232
shongCoder <soyeong163@gmail.com>
2024-10-25 11:23:42 +0900

    first commit

--- src/main/java/org/zerok/d2/controller/SampleController.java
+++ src/main/java/org/zerok/d2/controller/SampleController.java
@@ -1,2 +1,21 @@
-package org.zerok.d2.controller;public class SampleController {
+package org.zerok.d2.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sample")
+@RequiredArgsConstructor
+@Log4j2
+public class SampleController {
+
+    @GetMapping("hello")
+    public String[] hello() {
+        log.info("hello");
+        return new String[] {"hello Shong"};
+    }
+
 }
